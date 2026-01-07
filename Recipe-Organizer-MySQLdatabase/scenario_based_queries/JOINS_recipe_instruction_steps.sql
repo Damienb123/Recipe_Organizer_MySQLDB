7585e19c2daff95cf15f7eeba12ffe8738123b1d
DebugDamien <119611108+Damienb123@users.noreply.github.com>
2026-01-06 22:36:21 -0600

    JOINS clause
    implements a situation where the user wants to find a gathered list of steps each recipe has.

---
+++ Recipe-Organizer-MySQLdatabase/scenario_based_queries/JOINS_recipe_instruction_steps.sql
@@ -0,0 +1,8 @@
+SELECT 
+	Recipes.title,
+    Instructions.recipe_id,
+    Instructions.step_number,
+    Instructions.instruction_text
+FROM Recipes
+INNER JOIN Instructions ON
+Recipes.recipe_id=Instructions.recipe_id
